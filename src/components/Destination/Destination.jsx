bb8f09bfa6d4706651e09175b337c2ff020d9984
MANOTM <otmaneolmansouri1@gmail.com>
2023-07-25 20:54:53 +0100

    Finish

--- src/components/Destination/Destination.jsx
+++ src/components/Destination/Destination.jsx
@@ -15,14 +15,14 @@ export default function Destination() {
     <div className='destination my-shadow p-5 rounded-md' >
         <div className='font-semibold text-lg pb-3 ml-4'>Destination</div>
         <div className="flex gap-2 flex-wrap">
-            <Objet label="website URL" title='Website' Icon={<LinkIcon/>}/>
+            <Objet label="Website URL" title='Website' Icon={<LinkIcon/>}/>
             <Objet label="URL of document" title='Google Docs' Icon={<DocsIcon/>}/>
-            <Objet label="chaine youtube" title='Youtube' Icon={<YoutubIcon/>}/> 
-            <Objet label="facebook URL" title='Facebook' Icon={<FacebookIcon/>}/> 
-            <Objet label="upload link" title='Upload' Icon={<UploadIcon/>}/> 
-            <Objet label="instagram profile" title='Instagram' Icon={<InstagramIcon/>}/> 
-            <Objet label="message" title='Message' Icon={<MessageIcon/>}/> 
-            <Objet label="email" title='Email' Icon={<EmailIcon/>}/> 
+            <Objet label="Chaine youtube" title='Youtube' Icon={<YoutubIcon/>}/> 
+            <Objet label="Facebook URL" title='Facebook' Icon={<FacebookIcon/>}/> 
+            <Objet label="Upload link" title='Upload' Icon={<UploadIcon/>}/> 
+            <Objet label="Instagram profile URL" title='Instagram' Icon={<InstagramIcon/>}/> 
+            <Objet label="Message" title='Message' Icon={<MessageIcon/>}/> 
+            <Objet label="Email" title='Email' Icon={<EmailIcon/>}/> 
             <Objet label="Wifi key" title='Wifi' Icon={<WifiIcon/>}/> 
         </div>
     </div>
