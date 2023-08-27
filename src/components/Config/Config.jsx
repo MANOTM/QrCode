016f319ae139c747ab05b471776f0fe1a487a8fa
MANOTM <otmaneolmansouri1@gmail.com>
2023-08-27 21:39:49 +0100

    what i offer section

--- src/components/Config/Config.jsx
+++ src/components/Config/Config.jsx
@@ -1,12 +1,23 @@
 import './config.css'
 import patter from '../../data/pattern.json'
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
+import { motion } from 'framer-motion'
 import { context } from '../../context/QrContext'
 export const Config = () => {
+    const [ShowToast,setShowToast]=useState(false)
+    useEffect(()=>{
+        if(ShowToast){
+            setTimeout(() => {
+                setShowToast(false)
+            }, 1300);
+        }
+    },[ShowToast])
     const { colors, currentColor, setImageType, imgTypes, currentImgType, setCorrentColor } = useContext(context)
-
     return (
         <div className='rounded-md config w-[33%] my-shadow config p-5'>
+           {ShowToast && <motion.div    animate={{opacity:[0,1,1,1,0],y:-10}}   transition={{ ease: "easeOut", duration: 1.2 }} className="fixed bottom-44 left-[38%] bg-sky-500 text-white p-2 rounded-md ">
+           Sorry, we don't have that yet
+            </motion.div> } 
             <div>
                 <div className='flex flex-wrap gap-4'>
                 <div className='my-w-resp'>
@@ -14,7 +25,7 @@ export const Config = () => {
                     <div className="flex gap-6 flex-wrap pattern">
                         {patter.map((val, key) => {
                             return (
-                                <div className="rounded-md border opacity-[.8]" key={key}>
+                                <div className="rounded-md border opacity-[.8]" key={key} onClick={()=>setShowToast(true)}>
                                     <img src={val.src} className="w-12 p-2" />
                                 </div>)
                         })}
@@ -38,8 +49,9 @@ export const Config = () => {
                 <div className='font-semibold text-lg pb-3 mt-7'>Image Format</div>
                 <div className="flex gap-6 flex-wrap ">
                     {imgTypes.map((val, key) => {
+
                         return (
-                            <div className={`rounded-md border p-2 cursor-pointer opacity-80 ${currentImgType == val && 'active'}`} key={key} onClick={() => setImageType(val.toLocaleLowerCase())}>
+                            <div className={`rounded-md border p-2 cursor-pointer opacity-80 ${currentImgType == val && 'active'}`} key={key} onClick={() => setImageType(val)}>
                                 <div className='w-8 h-5 rounded-md'>
                                     {val}
                                 </div>
